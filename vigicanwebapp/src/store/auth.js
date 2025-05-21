cf90d91422e339fa401f24411e7a6881c4870d64
Ugwoke Edward Chinedu <nedu.stack@gmail.com>
2025-05-21 17:12:49 +0100

    Project Setup

---
+++ vigicanwebapp/src/store/auth.js
@@ -0,0 +1,23 @@
+import { create } from "zustand";
+import { mountStoreDevtool } from "simple-zustand-devtools";
+
+const useAuthStore = create((set, get) => ({
+  allUserData: null,
+  loading: false,
+
+  user: () => ({
+    user_id: get().allUserData?.user_id || null,
+    username: get().allUserData?.username || null,
+  }),
+
+  setUser: (user) => set({ allUserData: user }),
+  setLoading: (loading) => set({ loading }),
+  clearUser: () => set({ allUserData: null }),
+  setLoggedIn: () => get().allUserData !== null,
+}));
+
+if (import.meta.env.MODE === "development") {
+  mountStoreDevtool("Store", useAuthStore);
+}
+
+export default { useAuthStore };
