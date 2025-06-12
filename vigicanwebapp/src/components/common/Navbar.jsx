34a5badc1031c62eb18957380b559e0ec759b4aa
Ugwoke Edward Chinedu <nedu.stack@gmail.com>
2025-06-13 00:08:47 +0100

    Landing Page update

--- vigicanwebapp/src/components/common/Navbar.jsx
+++ vigicanwebapp/src/components/common/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../../styles/Navbar.css";
-import logo from "../../assets/images/VIGICAlogo.png";
+import logo from "../../assets/images/vigica.png";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
